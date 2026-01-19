9a43ea2da10fb9fb5c0d2511628c4e8f5ba6e68e
wings_Feiglein <3867661377@qq.com>
2026-01-19 21:08:09 +0800

    开发认证授权模块

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from flask import Flask
 #from app.extensions import init_extensions, db
 from config import config_dict
 from extensions import init_extensions, db
+from fastapi import FastAPI
+from routes import user
 import os
 
 def create_app(config_name="dev"):
@@ -29,6 +31,16 @@ def create_app(config_name="dev"):
 
     return app
 
+app = FastAPI(title="江苏省城市足球联赛后端API", version="1.0.0")
+app.include_router(user.router)
+
 if __name__ == "__main__":
-    app = create_app(config_name=os.getenv("FLASK_ENV", "dev"))
-    app.run(host="0.0.0.0", port=5000, debug=app.config["DEBUG"])
+    #app = create_app(config_name=os.getenv("FLASK_ENV", "dev"))
+    #app.run(host="0.0.0.0", port=5000, debug=app.config["DEBUG"])
+    import uvicorn
+    uvicorn.run(
+        app="main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True
+    )
