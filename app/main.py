a0eeef6d6db774481e2e132e1ad8461ce0dcf2db
vwo99 <a18061292806@163.com>
2026-01-24 20:34:35 +0800

    version 2.3.0 Completion

--- app/main.py
+++ app/main.py
@@ -1,32 +1,16 @@
-from flask import Flask
-from app.config import config_dict
-from app.extensions import init_extensions, db
+from app import create_app
+from extensions import socketio
 import os
 
-def create_app(config_name="dev"):
-    """创建并初始化Flask应用（纯净版，无冗余校验）"""
-    app = Flask(__name__)
-    # 加载配置
-    config = config_dict.get(config_name, config_dict["dev"])
-    app.config.from_object(config)
-    print(f"当前环境：{'开发环境' if config_name == 'dev' else '生产环境'}")
-    # 初始化数据库
-    init_extensions(app)
-    # 数据库连接测试【修复SQLAlchemy2.0语法，最简写法，零报错】
-    with app.app_context():
-        try:
-            db.session.execute(db.text("SELECT 1"))  # flask-sqlalchemy原生适配写法
-            db.session.commit()
-            print("✅ MySQL数据库连接成功！")
-        except Exception as e:
-            raise Exception(f"❌ 数据库连接失败：{str(e)}")
-    # 根路由
-    @app.route("/")
-    def index():
-        return {"code": 200, "message": "足球联赛后端服务启动成功"}
 
-    return app
+# ==============================================
+#                 主程序入口
+# ==============================================
 
-if __name__ == "__main__":
-    app = create_app(config_name=os.getenv("FLASK_ENV", "dev"))
-    app.run(host="0.0.0.0", port=5000, debug=app.config["DEBUG"])
+if __name__ == '__main__':
+    #1. 获取环境配置
+    env = os.getenv("FLASK_ENV", "dev")
+    app = create_app(config_name=env)
+    #2. 启动服务
+    print(f"服务正在启动，port=5000...")
+    socketio.run(app, host="0.0.0.0", port=5000,debug=True,allow_unsafe_werkzeug=True)
---
+++ app/test_jwt.py
@@ -0,0 +1,33 @@
+import jwt
+import time
+from datetime import datetime, timedelta
+from config import Config
+from utils.jwt_utils import create_token, verify_token
+
+def test_jwt():
+    try:
+        user_id = 1
+        expire_hours = 0.001
+        token = create_token(user_id, expire_hours)
+        #确认生成的Token正常
+        verify_id = verify_token(token)
+        if user_id == verify_id:
+            print("生成的Token正常")
+        #过期后验证过期的Token
+        time.sleep(5)
+        verify_id = verify_token(token)
+        if verify_id == None:
+            print("verify_id为None")
+        #验证无效Token
+        verify_id = verify_token("abc.token.invalid")
+        if verify_id == None:
+            print("verify_id为None")
+    except jwt.ExpiredSignatureError:
+        print("JWT Token已过期")
+    except jwt.InvalidTokenError:
+        print("JWT Token无效")
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+    test_jwt()
