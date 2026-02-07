f746e7605286ae72433585aeced36a3fca9d5877
wings_Feiglein <3867661377@qq.com>
2026-02-07 18:28:44 +0800

    在表中加入__init__函数来解决创建数据时的类型报错

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,6 +1,8 @@
+from re import A
 from app.models.base_model import BaseModel
 from app.extensions import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from typing import Optional, Any
 
 class User(BaseModel):
     """用户表"""
@@ -12,6 +14,21 @@ class User(BaseModel):
     focus_teams = db.Column(db.String(255), default="", comment="关注球队ID（逗号分隔）")
     score = db.Column(db.Integer, default=0, comment="球迷积分")
     avatar_url = db.Column(db.String(255), nullable=True, comment="头像URL")
+
+    def __init__(self, username: str, 
+                 password: str = "", 
+                 openid: Optional[str] = None, 
+                 focus_teams: Optional[str] = None, 
+                 score: Optional[int] = None, 
+                 avatar_url: Optional[str] = None, 
+                 **kwargs):
+        self.username = username
+        self.password = password
+        self.openid = openid
+        self.focus_teams = focus_teams
+        self.score = score
+        self.avatar_url = avatar_url
+        super().__init__(**kwargs)
 
     # 密码加密（存储时调用）
     def set_password(self, password):
@@ -29,3 +46,11 @@ class UserCollection(BaseModel):
     match_id = db.Column(db.Integer, db.ForeignKey("match.id", ondelete="CASCADE"), comment="赛事ID")
     # 联合唯一约束：同一用户不能重复收藏同一赛事
     __table_args__ = (db.UniqueConstraint("user_id", "match_id", name="uk_user_match"),)
+
+    def __init__(self,
+                 user_id: int,
+                 match_id: int,
+                 **kwargs):
+        self.user_id = user_id
+        self.match_id = match_id
+        super().__init__(**kwargs)
