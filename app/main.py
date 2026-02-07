b926f7685187a290da8e76005d70ec6d5284d7af
wings_Feiglein <3867661377@qq.com>
2026-02-07 16:18:21 +0800

    重新测试test_db.py成功

--- app/main.py
+++ app/main.py
@@ -1,3 +1,8 @@
+# 将当前文件所在目录的父目录加入路径
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+
 from app import create_app
 from extensions import socketio
 import os
@@ -13,4 +18,5 @@ if __name__ == '__main__':
     app = create_app(config_name=env)
     #2. 启动服务
     print(f"服务正在启动，port=5000...")
+    socketio = app.extensions['socketio'] # 获取已初始化的socketio实例
     socketio.run(app, host="0.0.0.0", port=5000,debug=True,allow_unsafe_werkzeug=True)
