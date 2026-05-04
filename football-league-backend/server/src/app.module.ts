033a3c83e91674fdbce1bd9f95631c4351619a32
wings_Feiglein <3867661377@qq.com>
2026-05-04 16:58:11 +0800

    feat: 增加聊天室的后端接口，以及为不同的比赛增加了独立的聊天室

--- football-league-backend/server/src/app.module.ts
+++ football-league-backend/server/src/app.module.ts
@@ -13,6 +13,7 @@ import { MatchModule } from './match/match.module';
 import { NewsModule } from './news/news.module';
 import { SocialModule } from './social/social.module';
 import { TasksModule } from './tasks/tasks.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
@@ -31,6 +32,7 @@ import { TasksModule } from './tasks/tasks.module';
     NewsModule,
     SocialModule,
     TasksModule,
+    ChatModule,
   ],
   controllers: [AppController],
   providers: [AppService],
