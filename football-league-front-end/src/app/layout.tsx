6b291e5885dbfc488760ebcc6e8db46d2c3039c2
wings_Feiglein <3867661377@qq.com>
2026-05-04 10:46:24 +0800

    Revert "feat(frontend): 初步添加竞猜部分的前端功能"

--- football-league-front-end/src/app/layout.tsx
+++ football-league-front-end/src/app/layout.tsx
@@ -31,8 +31,8 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
-          enableSystem={false}
+          defaultTheme="system"
+          enableSystem
           disableTransitionOnChange
         >
           {children}
